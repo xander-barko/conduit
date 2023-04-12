3d5cf7639de13a43c1e11ff152cb00cb167da224
xander-barko <barkoczis@gmail.com>
2023-04-12 10:41:20 +0200

    accept cookies, comments added

--- test/test_conduit.py
+++ test/test_conduit.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
+
 class TestConduit(object):
     def setup_method(self):
         service = Service(executable_path=ChromeDriverManager().install())
@@ -18,55 +19,68 @@ class TestConduit(object):
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('window-position=-1000,0')
         self.browser = webdriver.Chrome(service=service, options=options)
 
         URL = "http://localhost:1667/#/"
         self.browser.get(URL)
         self.browser.maximize_window()
+
     def teardown_method(self):
         pass
-        # self.browser.quit()
+        self.browser.quit()
 
-    # def test_apply_privacy_statement_as_cookies(self):
-    #     cookie_accept_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')))
-    #     cookie_accept_btn.click()
-    #     time.sleep(2)
-    #     cookie_div = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located((By.ID, 'cookie-policy-panel')))
-    #     assert not cookie_div.is_displayed()
-    #
+    # Adatkezelési nyilatkozat használata - cookie-k elfogadása
+    def test_apply_privacy_statement_as_cookies(self):
+        cookie_accept_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, 'button[class="cookie__bar__buttons__button cookie__bar__buttons__button--accept"]')))
+        cookie_accept_btn.click()
+        cookie_panel = self.browser.find_elements(By.ID, 'cookie-policy-panel')
+        assert len(cookie_panel) == 0
+
+    # Regisztráció érvényes adatokkal
     def test_sign_up(self):
-        sign_up_button = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="#/register"]')))
+        sign_up_button = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="#/register"]')))
         sign_up_button.click()
-        username_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Username"]')))
+        username_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Username"]')))
         username_input.send_keys(sign_up_data[0])
-        email_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Email"]')))
+        email_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Email"]')))
         email_input.send_keys(sign_up_data[1])
-        password_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Password"]')))
+        password_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Password"]')))
         password_input.send_keys(sign_up_data[2])
-        submit_sign_up = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary pull-xs-right"]')))
+        submit_sign_up = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary pull-xs-right"]')))
         submit_sign_up.click()
         time.sleep(2)
-        success_report = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="swal-text"]')))
+        success_report = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'div[class="swal-text"]')))
         assert success_report.text == "Your registration was successful!"
-        successful_ok_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="swal-button swal-button--confirm"]')))
+        successful_ok_btn = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="swal-button swal-button--confirm"]')))
         successful_ok_btn.click()
 
+    # Bejelentkezés érvényes adatokkal
     def test_sign_in(self):
-        sign_in_header_button = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="#/login"]')))
+        sign_in_header_button = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href="#/login"]')))
         sign_in_header_button.click()
-        sign_in_email_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Email"]')))
+        sign_in_email_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Email"]')))
         sign_in_email_input.send_keys(sign_up_data[1])
-        sign_in_password_input = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Password"]')))
+        sign_in_password_input = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Password"]')))
         sign_in_password_input.send_keys(sign_up_data[2])
-        sign_in_btn = WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary pull-xs-right"]')))
+        sign_in_btn = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[class="btn btn-lg btn-primary pull-xs-right"]')))
         sign_in_btn.click()
         time.sleep(2)
-        header_menus = WebDriverWait(self.browser, 5).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[class="nav-link"]')))
+        header_menus = WebDriverWait(self.browser, 5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[class="nav-link"]')))
         name_text = header_menus[2]
         assert name_text.text == sign_up_data[0]
-
-
 
     # def test_listed_datas(self):
     #     pass
@@ -91,4 +105,3 @@ class TestConduit(object):
     #
     # def test_sign_out(self):
     #     pass
-
